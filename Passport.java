1803c4d80d8d217b312b5477b9454e20a9c60825
J1yNa <127597687+J1yNa@users.noreply.github.com>
2025-10-13 19:57:26 +0300

    Initial commit. Added the image file

---
+++ Passport.java
@@ -0,0 +1,15 @@
+public class Passport {
+    public static void main(String[] args) {
+        // Вывод ФИО и группы
+        System.out.println("ФИО: Потапов Денис Матвеевич");
+        System.out.println("Группа: ИБ1-23");
+        System.out.println();
+
+        // Информация о себе
+        System.out.println("О себе:Меня зовут Денис,я учусь на программиста");
+        System.out.println("- Изучаю Java и Python программирование");
+        System.out.println("- Увлекаюсь спортом");
+        System.out.println("- Люблю рисовать в paint");
+        System.out.println("- Цель: быть хорошим it-специалистом ");
+    }
+}
